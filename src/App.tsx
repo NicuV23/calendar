e6cb2ee8b980db7a8c57a2168bdce085447cf814
NicuV23 <v.nicu213@gmail.com>
2024-02-12 17:40:19 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+interface CounterProps {}
+
+interface DateProps {
+  date: Date;
+}
+
+const Counter: React.FC<CounterProps> = () => {
+  const [step, setStep] = useState<number>(0);
+  const [count, setCount] = useState<number>(0);
+
+  const date: Date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div className="display">
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step:{step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        Count:{count}
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <div className="date">
+        <span>
+          {count === 0
+            ? `Today is `
+            : count > 0
+            ? `${count} days from Today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
+    </div>
+  );
+};
